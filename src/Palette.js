9f5bf5d6c112dabd9f466c64bd92fb7570f333cc
ThuraAungKyaw <thuraaungkyaw9@gmail.com>
2022-05-25 23:51:06 +0630

    palette intensity slider added

--- src/Palette.js
+++ src/Palette.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
-import { generatePalette } from "./colorHelpers";
+import Slider from "rc-slider";
+import 'rc-slider/assets/index.css';
 import './Palette.css';
-import seedColors from "./seedColors";
+
 class Palette extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            colorIntensity: 500
+        }
+
+        this.handleSliderChange = this.handleSliderChange.bind(this);
+    }
+
+    handleSliderChange(value) {
+        this.setState({ colorIntensity: value })
+    }
     render() {
-        console.log(generatePalette(seedColors[4]))
-        const colorBoxes = this.props.colors.map(color => {
-            return <ColorBox background={color.color} name={color.name} />
+        const { colors } = this.props.palette;
+        const { colorIntensity } = this.state;
+        const colorBoxes = colors[colorIntensity].map(color => {
+            return <ColorBox key={color.id} background={color.hex} name={color.name} />
         })
         return (
             <div className="Palette">
+                <Slider min={100} max={900} step={100} value={colorIntensity} onChange={this.handleSliderChange} />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
