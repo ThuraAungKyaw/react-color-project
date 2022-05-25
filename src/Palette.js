21fa2c1bc6e9cc9ff066eb7704e79115ee4dd2d0
ThuraAungKyaw <thuraaungkyaw9@gmail.com>
2022-05-25 23:27:03 +0630

    Add color helper methods to generate colors

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
+import { generatePalette } from "./colorHelpers";
 import './Palette.css';
+import seedColors from "./seedColors";
 class Palette extends Component {
     render() {
+        console.log(generatePalette(seedColors[4]))
         const colorBoxes = this.props.colors.map(color => {
             return <ColorBox background={color.color} name={color.name} />
         })
